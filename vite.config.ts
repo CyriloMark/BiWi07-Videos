ccf309fc0f065be525b05e6aeb33bfccc0e4dfc2
CyriloMark <cyrilo.mark@gmail.com>
2026-01-20 12:22:50 +0100

    first Github Pages Deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+import { resolve } from "path";
 
 export default defineConfig({
     plugins: [tailwindcss()],
     base: "/BiWi07-Videos/",
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, "index.html"),
+                video: resolve(__dirname, "video.html"),
+            },
+        },
+    },
 });
