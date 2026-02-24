28febaed660ed692f24511bb128a55ca951a0b3c
CyriloMark <cyrilo.mark@gmail.com>
2026-02-24 11:20:07 +0100

    Updates

---
+++ assets/download_button-BziyS8TB.js
@@ -0,0 +1,30 @@
+(function(){const i=document.createElement("link").relList;if(i&&i.supports&&i.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))s(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const o of t.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&s(o)}).observe(document,{childList:!0,subtree:!0});function l(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function s(e){if(e.ep)return;e.ep=!0;const t=l(e);fetch(e.href,t)}})();const r="/BiWi07-Videos/assets/IMG_5500-H7IIrrqH.png",u={INTRO:{id:"INTRO",title:"Einstieg und Vorstellung",sub:"Einführung - Warum befassen wir uns mit Videobeiträgen?",href:"https://drive.google.com/file/d/15UhbZoX2ojD4bEujj1OeSnLRxRok0bhG/preview",key:"intro",icon:r,index:1},DEF:{id:"DEF",title:"Definitionen",sub:"Definitionen einer Kulturtechnik und Videobeiträgen",href:"https://www.youtube.com/watch?v=146GL3pyKDY&list=RD146GL3pyKDY&start_radio=1",key:"def",icon:r,index:2},MEANING:{id:"MEANING",title:"Bedeutung für Menschen und Lernende",sub:"Bedeutung für Schülerinnen und Schüler - Bedeutung für Jugendliche - Bedeutung für Menschen",href:"https://drive.google.com/file/d/15KewuU6LFxVzXvnJRe3ZPNB28t1tY7i6/preview",key:"meaning",icon:r,index:3},IMPLEMENTATION:{id:"IMPLEMENTATION",title:"Technische Umsetzung",sub:"Technische Umsetzung - Videobeiträge im Kontext Lernen - Einsatzmöglichkeiten - Gute vs. schlechte Lernvideos",href:"",key:"implementation",icon:r,index:4},DISCUSSION:{id:"DISCUSSION",title:"Diskussion, Pro und Contra",sub:"Vor- und Nachteile für den Unterricht - Falschinformationen - Vorgefertigte Meinungen und Identitäten - Aufmerksamkeit",href:"",key:"discussion",icon:r,index:5},OUTRO:{id:"OUTRO",title:"Fazit",sub:"Sind Videos eine neue Kulturtechnik oder eine Ergänzung?",href:"",key:"outro",icon:r,index:6}};function c(n){const i=u[n];return console.log(i.title),i}function d({data:n,key:i}){return`
+        <a
+            key={${i}}
+            href="${n.href}"
+            target="_blank"
+            rel="noopener noreferrer">
+            <div class="w-full flex flex-row justify-between drop-shadow-2xl items-center pl-5 pr-5 pt-2 pb-2 border border-black rounded-3xl bg-white">
+                <div class="flex flex-row items-center>
+                    ${n.icon?'<div class="w-12 aspect-square bg-amber-500"></div>':""}
+                    <p
+                        class={"inter-regular text-lg text-black ${n.icon?"ml-2":""}"}>
+                        ${n.title}
+                    </p>
+                </div>
+
+                <div class="w-12 p-1 aspect-square">
+                    <a
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        href={${n.href}}>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 122.88 122.88">
+                            <path d="M61.44,0A61.46,61.46,0,1,1,18,18,61.21,61.21,0,0,1,61.44,0Zm10,50.74A3.31,3.31,0,0,1,76,55.47L63.44,67.91a3.31,3.31,0,0,1-4.65,0L46.38,55.65A3.32,3.32,0,0,1,51,50.92l6.83,6.77.06-23.84a3.32,3.32,0,0,1,6.64.06l-.07,23.65,6.9-6.82ZM35,81.19l0-13a3.32,3.32,0,0,1,6.64.06l0,9.45q19.76,0,39.5,0l0-9.51a3.32,3.32,0,1,1,6.64.06l0,12.91h0a3.32,3.32,0,0,1-3.29,3.17q-23.08,0-46.15,0A3.32,3.32,0,0,1,35,81.19ZM99.44,23.44a53.74,53.74,0,1,0,15.74,38,53.58,53.58,0,0,0-15.74-38Z" />
+                        </svg>
+                    </a>
+                </div>
+            </div>
+        </a>
+    `}export{d as D,c as g,u as l};
---
+++ assets/video-BQl7qDJO.js
@@ -0,0 +1,25 @@
+import{g as o,D as i}from"./download_button-BziyS8TB.js";const d=new URLSearchParams(window.location.search),r=d.get("v"),t=o(r||"INTRO");document.querySelector("#videoWrapper").innerHTML=`
+    <h4 class="inter-sm text-black md:leading-8 md:text-xl text-lg">
+        Lernvideo ${t.index}
+    </h4>
+    <h3 class="inter-bold italic text-black md:leading-8 md:text-3xl text-xl mt-5">
+        ${t.title}
+    </h3>
+
+    <h4 class="inter-sm text-black md:leading-8 md:text-xl text-lg mt-2">
+        ${t.sub}
+    </h4>
+
+    <div class="w-[80%] p-1 mt-5 drop-shadow-2xl border-2 border-white rounded-4xl overflow-visible">
+        <div class="w-full aspect-video bg-white rounded-3xl overflow-hidden">
+            <iframe
+                src=${t.href}
+                title="Google Drive video"
+                loading="lazy"
+                allow="autoplay"
+                sandbox="allow-scripts allow-same-origin"
+                class="w-full h-full"
+            />
+        </div>
+    </div>
+`;document.querySelectorAll("#_download").forEach((e,l)=>{const a={title:e.attributes.getNamedItem("title")?.value||"",href:e.attributes.getNamedItem("url")?.value||"",icon:e.attributes.getNamedItem("icon")?.value};e.innerHTML=i({data:a,key:l.toString()})});
