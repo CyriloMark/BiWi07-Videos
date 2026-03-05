b3152c0c5395f8808bf15e945d15d4381ad1bdac
CyriloMark <cyrilo.mark@gmail.com>
2026-03-05 19:53:10 +0100

    Updates

--- assets/video-D0yPimcS.js
+++ assets/video-D0yPimcS.js
@@ -1,4 +1,4 @@
-import{g as a,D as i}from"./download_button-C2QBDdyI.js";function o({data:e,key:t}){return`
+import{g as o,D as r}from"./download_button-CmaImjqy.js";function a({data:e,key:t}){return`
         <div
             key={${t}}
             data-key="${t}"
@@ -7,22 +7,22 @@ import{g as a,D as i}from"./download_button-C2QBDdyI.js";function o({data:e,key:
             <p class="inter-bold md:text-lg text-sm text-black">${d(e.time)}</p>
             <p class="inter-regular md:text-lg text-sm text-black md:ml-2 ml-1">${e.title}</p>
         </div>
-    `}function d(e){let t=Math.floor(e/60),r=e%60;return`${t}:${r<10?"0"+r:r}`}const s=new URLSearchParams(window.location.search),n=s.get("v"),l=a(n||"INTRO");document.querySelector("#videoWrapper").innerHTML=`
+    `}function d(e){let t=Math.floor(e/60),i=e%60;return`${t}:${i<10?"0"+i:i}`}const s=new URLSearchParams(window.location.search),n=s.get("v"),l=o(n||"INTRO");document.querySelector("#videoWrapper").innerHTML=`
     <h4 class="inter-sm text-black md:leading-8 md:text-xl text-lg">
-        Lernvideo ${l.index}
+        Lernvideo ${l.videos[0].index}
     </h4>
     <h3 class="inter-bold italic text-black md:leading-8 md:text-3xl text-xl mt-5">
-        ${l.title}
+        ${l.videos[0].title}
     </h3>
 
     <h4 class="inter-sm text-black md:leading-8 leading-6 text-center md:text-xl text-lg mt-2">
-        ${l.sub}
+        ${l.videos[0].sub}
     </h4>
 
     <div class="w-[80%] p-1 mt-5 drop-shadow-2xl border-2 border-white rounded-4xl overflow-visible">
         <div class="w-full aspect-video bg-white rounded-3xl overflow-hidden">
             <iframe
-                src=${l.href}
+                src=${l.videos[0].href}
                 title="Google Drive video"
                 loading="lazy"
                 allow="autoplay"
@@ -32,11 +32,11 @@ import{g as a,D as i}from"./download_button-C2QBDdyI.js";function o({data:e,key:
             />
         </div>
     </div>
-`;l.timestamps.length>0&&(document.querySelector("#timestampWrapper").innerHTML=`
+`;l.videos[0].timestamps.length>0&&(document.querySelector("#timestampWrapper").innerHTML=`
         <h3 class="inter-bold italic text-black md:leading-8 md:text-3xl text-xl mt-12">
             Zeitstempel
         </h3>
         <div class="mt-5 w-[90%] md:w-[80%] flex flex-row flex-wrap">
-            ${l.timestamps.map((e,t)=>o({data:e,key:t.toString()})).join("")}
+            ${l.videos[0].timestamps.map((e,t)=>a({data:e,key:t.toString()})).join("")}
         </div>
-    `);document.querySelectorAll("#_download").forEach((e,t)=>{const r={title:e.attributes.getNamedItem("title")?.value||"",href:l.transcript_href,icon:e.attributes.getNamedItem("icon")?.value};e.innerHTML=i({data:r,key:t.toString()})});
+    `);document.querySelectorAll("#_download").forEach((e,t)=>{const i={title:e.attributes.getNamedItem("title")?.value||"",href:l.transcript_href,icon:e.attributes.getNamedItem("icon")?.value};e.innerHTML=r({data:i,key:t.toString()})});
